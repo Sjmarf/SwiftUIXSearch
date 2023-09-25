29a5afd206bfbd25fd29b4cfabb15be1c4a2852a
Sjmarf <78750526+Sjmarf@users.noreply.github.com>
2023-09-25 18:06:47 +0100

    Initial Commit

---
+++ SwiftUIXSearch/SwiftUIXSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIXSearchApp.swift
+//  SwiftUIXSearch
+//
+//  Created by Sam Marfleet on 25/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIXSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
